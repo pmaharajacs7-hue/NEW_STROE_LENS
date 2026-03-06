a1988d2fc14d7fc3cde210d96fb8d21c016f591f
Raja_byte <pmaharajacs7@gmail.com>
2026-03-06 08:52:25 +0530

    Update ProductService.java

--- src/main/java/com/store/service/ProductService.java
+++ src/main/java/com/store/service/ProductService.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
+import com.store.entity.Sale;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
